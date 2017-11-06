8a1185bd148bcf115eefa896d0a0cec59f153e13
sakshiRajput <30687259+sakshiRajput@users.noreply.github.com>
2017-11-06 15:23:30 +0530

    Delete ProfilePictureDao.java

--- ProfilePictureDao.java
+++
@@ -1,9 +0,0 @@
-package com.collab.CollaborationBack.Dao;
-
-import com.collab.CollaborationBack.model.ProfilePicture;
-
-public interface ProfilePictureDao {
-
-	void uploadpic(ProfilePicture profilepic);
-	ProfilePicture getProfilePic(String username);
-}
